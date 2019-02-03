46be5d204c518cd38673e5bcce52d72d1546afe6
Ssonic8 <gladunalexv@gmail.com>
2019-02-03 20:52:47 +0300

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$('#discount-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		center: true,
+		margin: 10,
+		navText: ["",""],
+		nav: true
+	});
+	$('#team-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		center: true,
+		margin: 10,
+		navText: ["",""],
+		nav: true
+	});
+	$('#docs-carousel').owlCarousel({
+		items: 3,
+		loop: true,
+		center: true,
+		margin: 10,
+		navText: ["",""],
+		nav: true
+	});
+});
